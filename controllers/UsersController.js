9ad990ab982fb7323309322365265cc523d56349
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-08 22:03:04 +0200

    Return email and id only from the created user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,29 +1,30 @@
-import dbClient from '../utils/db';
 import crypto from 'crypto';
+import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+    const { email, password } = req.body;
 
     if (!email) {
-      return res.status(400).send('Missing email');
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).send('Missing password');
+      return res.status(400).json({ error: 'Missing password' });
     }
 
     try {
-      if (!await dbClient.getUserByEmail(email)) {
-        return res.status(400).send('Already exist');
+      if (await dbClient.getUserByEmail(email)) {
+        return res.status(400).json({ error: 'Already exist' });
       }
 
       const hashedPassword = crypto.createHash('sha1');
       hashedPassword.update(password);
 
       const user = await dbClient.addUser({ email, password: hashedPassword });
-      const { password, ...userWithoutPassword } = user;
-      return res.status(201).json(userWithoutPassword);
+
+      const { insertedId } = user;
+
+      return res.status(201).json({ id: insertedId, email });
     } catch (err) {
       return res.status(500).send('Internal server error');
     }
