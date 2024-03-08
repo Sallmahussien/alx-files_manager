982263840e61004876e5b7352652f60412986531
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-08 16:14:14 +0200

    Implement app controller

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    if (dbClient.isAlive() && redisClient.isAlive()) {
+      return res.status(200).json({ "redis": true, "db": true });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+
+      return res.status(200).json({ users, files });
+    } catch(error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+}
+
+export default AppController;
