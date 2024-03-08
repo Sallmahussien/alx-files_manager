a80c8bb362bcf0cc65a1a9d176a1b7cbdb7dd5da
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-08 21:29:07 +0200

    Add queries for search by email and adding new user

--- utils/db.js
+++ utils/db.js
@@ -25,6 +25,16 @@ class DBClient {
     const filesCollection = this.client.db().collection('files');
     return filesCollection.countDocuments();
   }
+
+  async addUser(user) {
+    const usersCollection = await this.client.db().collection('users');
+    return await usersCollection.insertOne(user);
+  }
+
+  async getUserByEmail(email) {
+    const usersCollection = await this.client.db().collection('users');
+    return await usersCollection.findOne({email: email});
+  }
 }
 
 const dbClient = new DBClient();
