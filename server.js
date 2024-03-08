08d882ba90bc33e7fabf4ac9e4cd8902903a15f9
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-08 16:14:43 +0200

    Start the server

---
+++ server.js
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import express from 'express';
+import router from './routes/index';
+
+const PORT = process.env.PORT || '5000';
+
+config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
+
+export default app;
