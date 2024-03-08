00db8c20d8e2027cd5906e808d08bd26a4293f07
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-08 21:30:32 +0200

    Implement route for users

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appRouter from './AppRoutes';
+import userRoutes from './UserRoutes';
 
 const router = Router();
 
 router.use('/', appRouter);
+router.use('/', userRoutes);
 
 export default router;
