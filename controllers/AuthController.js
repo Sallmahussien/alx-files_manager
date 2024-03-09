34d9d308d6f3ab2b13ce33478af35bfac5cab30d
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-09 11:01:19 +0200

    Refactore auth and user controllers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,46 +1,32 @@
 import { v4 as uuidv4 } from 'uuid';
-import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
   static async getConnect(req, res) {
-    const authHeader = req.headers.authorization;
-    const base64Credentials = authHeader.split(' ')[1];
-    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const email = decodedCredentials.split(':')[0];
-
     try {
-      const user = await dbClient.getUserByEmail(email);
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
+      const { user } = req;
+      const userId = user._id.toString();
 
       const token = uuidv4();
-
-      const key = `auth_${token}`;
+      const redisKey = `auth_${token}`;
       const dayDuration = 3600 * 24;
-      redisClient.set(key, user._id.toString(), dayDuration);
+
+      await redisClient.set(redisKey, userId, dayDuration);
 
       return res.status(200).json({ token });
     } catch (err) {
-      console.log(err);
+      console.error(err);
       return res.status(500).send('Internal server error');
     }
   }
 
   static async getDisconnect(req, res) {
-    const tokenFromHeaders = req.headers['x-token'];
-    const key = `auth_${tokenFromHeaders}`;
-    const userId = redisClient.get(key);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
     try {
+      const key = req.redisKey;
       await redisClient.del(key);
       return res.status(204).send('');
     } catch (err) {
+      console.error(err);
       return res.status(500).send('Internal server error');
     }
   }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
-import sha1 from 'sha1';
+/* eslint-disable import/named */
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import { hashPwd } from '../utils/utils';
 
 class UsersController {
   static async postNew(req, res) {
@@ -18,36 +18,20 @@ class UsersController {
         return res.status(400).json({ error: 'Already exist' });
       }
 
-      const user = await dbClient.addUser({ email, password: sha1(password) });
+      const user = await dbClient.addUser({ email, password: hashPwd(password) });
 
       const { insertedId } = user;
 
       return res.status(201).json({ id: insertedId, email });
     } catch (err) {
+      console.error(err);
       return res.status(500).send('Internal server error');
     }
   }
 
   static async getMe(req, res) {
-    const tokenFromHeaders = req.headers['x-token'];
-    const key = `auth_${tokenFromHeaders}`;
-
-    try {
-      const userId = await redisClient.get(key);
-
-      console.log(userId);
-
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const user = await dbClient.getUserById(userId);
-      console.log(user);
-      return res.status(200).json(user);
-    } catch (err) {
-      console.log(err);
-      return res.status(500).send('Internal server error');
-    }
+    const { user } = req;
+    return res.status(200).json(user);
   }
 }
 
