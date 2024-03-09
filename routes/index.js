a12ab8a3370f3840b1a7f7bd9e4c8583c690a4c4
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-09 14:19:18 +0200

    Add file route in index file

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appRouter from './AppRoutes';
 import userRoutes from './UserRoutes';
 import authRouter from './AuthRoutes';
+import fileRouter from './FileRoutes';
 
 const router = Router();
 
 router.use('/', appRouter);
 router.use('/', userRoutes);
 router.use('/', authRouter);
+router.use('/', fileRouter);
 
 export default router;
