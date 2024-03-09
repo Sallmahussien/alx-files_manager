af6c24031fcf05bdc2fb3a8893594062f0e6511c
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-09 11:01:37 +0200

    Refactor auth and user routes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/named */
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import { xTokenAuth } from '../middlewares/auth';
 
 const userRouter = Router();
 
 userRouter.post('/users', UsersController.postNew);
-userRouter.get('/users/me', UsersController.getMe);
+userRouter.get('/users/me', xTokenAuth, UsersController.getMe);
 
 export default userRouter;
