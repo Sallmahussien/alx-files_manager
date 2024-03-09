3d024296c278198971241721da41e76ffd6009e5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-09 23:17:13 +0200

    Add routes for show and index files

--- routes/FileRoutes.js
+++ routes/FileRoutes.js
@@ -6,5 +6,7 @@ import { xTokenAuth } from '../middlewares/auth';
 const fileRouter = Router();
 
 fileRouter.post('/files', xTokenAuth, FilesController.postUpload);
+fileRouter.get('/files/:id', xTokenAuth, FilesController.getShow);
+fileRouter.get('/files', xTokenAuth, FilesController.getIndex);
 
 export default fileRouter;
