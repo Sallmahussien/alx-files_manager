8d627f39b05e968978a47543a2ae17d090c0f2b0
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-11 12:10:33 +0200

    Search using parent Id as a str

--- utils/db.js
+++ utils/db.js
@@ -67,7 +67,7 @@ class DBClient {
   async getPaginatedFiles(userId, parentId, page) {
     const filesCollection = await this.client.db().collection('files');
     const userIdObject = new ObjectID(userId);
-    const parentIdObject = parentId  ? new ObjectID(parentId) : null;
+    // const parentIdObject = parentId  ? new ObjectID(parentId) : null;
 
     const pageSize = 20;
     const skip = page * pageSize;
@@ -76,8 +76,8 @@ class DBClient {
       userId: userIdObject,
     };
 
-    if (parentIdObject) {
-      matchStage.parentId = parentIdObject;
+    if (parentId) {
+      matchStage.parentId = parentId;
     }
 
     const pipeline = [
